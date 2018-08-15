d1a1940ace3c5d629fdccdbabf7a2844688b827f
Angela Lu <angela.lu0919@gmail.com>
2018-08-15 00:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function appendKitten(name) {
 }
 
 function prependKitten(name) {
-  kittens.splice(0, 0, name)
-  return kittens
+  var kitties = kittens.splice(0, 0, name)
+  return kitties
 }
 
 function removeLastKitten() {
