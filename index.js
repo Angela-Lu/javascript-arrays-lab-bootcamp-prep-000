3f3168a70c4d78aad18e2a171440c7659cc0c1e2
Angela Lu <angela.lu0919@gmail.com>
2018-08-15 00:02:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your functions and code here
+let kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(name) {
+  return kittens.concat(name)
+}
+
+function prependKitten(name) {
+  kittens.splice(0, 0, name)
+  return kittens
+}
+
+function removeLastKitten() {
+  kittens.slice(-1)
+  return kittens
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
